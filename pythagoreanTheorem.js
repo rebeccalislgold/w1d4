c6282872ef3c7a13923974de2c480531f02e2150
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-05-02 17:38:14 +0000

    Array Mapping Challenge

---
+++ pythagoreanTheorem.js
@@ -0,0 +1,23 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(a => hypotenuse(a.x, a.y));
+
+function squareIt(number) {
+
+   return number * number;
+
+}
+
+function hypotenuse(c, d) {
+
+  return Math.sqrt(squareIt(c) + squareIt(d));
+
+}
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
