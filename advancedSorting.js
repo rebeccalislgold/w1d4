c5b8cfbacfd1366aedf568f74728bd42d89ee871
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-05-02 19:08:59 +0000

    Sort Objects in Array - Two Levels

---
+++ advancedSorting.js
@@ -0,0 +1,29 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+function compareName(a, b){
+
+  var x = a.name;
+  var y = b.name;
+  if (x < y) {return -1;}
+  if (x > y) {return 1;}
+  return 0;
+
+};
+
+function compareAge(a, b){
+
+  return b.age - a.age
+
+};
+
+let sortedByAge = students.sort(compareAge)
+let sortedByName = students.sort(compareName)
+
+// console.log(students);
+
+console.log(students.sort(compareName));
