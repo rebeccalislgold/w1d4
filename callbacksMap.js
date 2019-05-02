5689657846b0d2251d1e6edc73f08ce1164c0bc0
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-05-02 20:56:31 +0000

    Own version of built-in array map function

---
+++ callbacksMap.js
@@ -0,0 +1,27 @@
+function map(someArray, someFunction) {
+
+  var newArray = [];
+
+  for (i = 0; i < someArray.length; i++) {
+
+    newArray[i] = someFunction(someArray[i])
+
+  }
+
+  console.log(newArray);
+
+}
+
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
